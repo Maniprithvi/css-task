fb1e0289d9b700142d1fa4096671755e1c983d84
Maniprithvi <007maniprithvi@gmail.com>
2023-09-22 18:15:18 +0530

    initial task

---
+++ map/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { MapContainer,TileLayer } from 'react-leaflet'
+
+const App = () => {
+  return (
+    // <MapContainer center={[41.40338, 2.17403]} zoom={13}>
+
+    //   <TileLayer 
+    //   attribution=''
+    //   url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" 
+
+    //   />
+    // </MapContainer>
+    <h1>hello</h1>
+  )
+}
+
+export default App
